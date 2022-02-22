c02c6097f153df727c5e5b52af2ccb866d741d21
vinicius-je <vinicius.je99@gmail.com>
2022-02-22 18:17:58 -0300

    selection sort

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from algorithms import selectionSort
 import random
 
 def main():
     list = random.sample(range(1, 100), 20)
-    print('before selection sort')
-    print(list)
-    selectionSort(list)
-    print('after selectio sort')
     print(list)
 
 if __name__ == '__main__':
--- selectionSort.py
+++ selectionSort.py
@@ -5,6 +5,7 @@ def changePosition(list, p1, p2):
 
 def selectionSort(list):
     list_length = len(list)
+
     for i in range(list_length - 1):
         min_index = i
         # find the lowest value index
@@ -13,5 +14,3 @@ def selectionSort(list):
                 min_index = j
         if list[i] > list[min_index]:
             changePosition(list, i, min_index)
-
-
